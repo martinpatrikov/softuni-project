8c0fff21b01841c55881f3d67ed60d0586dff84b
Martin Patrikov <mpatrikovjr@gmail.com>
2022-04-11 19:08:51 +0300

    Initial server commit

---
+++ server/middlewares/auth.js
@@ -0,0 +1,16 @@
+const { verifySession } = require('../services/users');
+
+module.exports = () => (req, res, next) => {
+    const token = req.headers['x-authorization'];
+
+    try {
+        if (token) {
+            const userData = verifySession(token);
+            req.user = userData;
+        }
+        next();
+        // Should delete the token on the front-end
+    } catch (err) {
+        res.status(498).json({ message: 'Invalid access token. Please sign in' });
+    }
+};
