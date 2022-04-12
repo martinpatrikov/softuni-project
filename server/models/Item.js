221110aa0bc0d302c0a3c30d2bbe36b405107a93
Martin Patrikov <mpatrikovjr@gmail.com>
2022-04-12 19:18:51 +0300

    First Item changes

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,19 +1,9 @@
 const { model, Schema, Types: { ObjectId } } = require('mongoose');
 
 const schema = new Schema({
-    make: { type: String, required: [true, 'Make is required'] },
-    model: { type: String, required: true },
-    year: {
-        type: Number,
-        required: true,
-        min: [1950, 'Year must be between 1950 and 2050'],
-        max: [2050, 'Year must be between 1950 and 2050']
-    },
-    description: { type: String, required: true, minlength: [10, 'Description must be at least 10 characters long'] },
-    price: { type: Number, required: true },
-    img: { type: String, required: true },
-    material: { type: String },
-    owner: { type: ObjectId, ref: 'User' }
+    name: { type: String, required: [true, 'Name is required'] },
+    artist: { type: String, required: [true, 'Artist is required'] },
+    file: { type: ObjectId, ref: 'Uploads.file' }
 });
 
 const Item = model('Item', schema);
